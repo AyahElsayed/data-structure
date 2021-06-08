e3c8488f8dd4379a2c83cca3076953f1d5566bb5
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-06-08 13:58:11 +0200

    linked list vs array

---
+++ linkedList/linkedListVs.array.cpp
@@ -0,0 +1,9 @@
+/*
+linked list                         |       array
+- flexible size                            - fixed size 
+- easy to add or delete item               - difficult to add or delete item
+- not necessary to keep                    - not necessary to keep sequential places in the memory
+  sequential places in the memory 
+- access item time slower                  - access item time faster
+
+*/
