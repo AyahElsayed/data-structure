36826577f58aff5113d19a58e2959153eed4ee88
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-06-08 15:22:44 +0200

    display linked list

--- linkedList/insertNode.cpp
+++ linkedList/insertNode.cpp
@@ -3,39 +3,65 @@ using namespace std;
 
 struct node
 {
-  int data;
-  node* next;
+    int data;
+    node *next;
 };
 
-node* head = NULL;
+node *head = NULL;
 
 void inserNode(int value);
+void display();
 
 int main()
 {
-    
-    
+    // display();
+    inserNode(5);
+    inserNode(10);
+    inserNode(15);
+    inserNode(7);
+    display();
+
     return 0;
 }
 
-void inserNode(int value){
-  node* new_node , *last;
-  new_node = new node ; // create pointer in one line
-  new_node-> data = value;
+void inserNode(int value)
+{
+    node *new_node, *last;
+    new_node = new node; // create pointer in one line
+    new_node->data = value;
 
-  // in case one node in the list 
-  if(head == NULL){
-    head = new_node;
-    new_node->next = NULL;
-  }
-  else{
-    last = head;
-    while (last->next != NULL)
+    // in case one node in the list
+    if (head == NULL)
     {
-      last = last->next;
+        head = new_node;
+        new_node->next = NULL;
     }
-    last->next = new_node;
-    new_node->next = NULL;
-    
-  }
+    else
+    {
+        last = head;
+        while (last->next != NULL)
+        {
+            last = last->next;
+        }
+        last->next = new_node;
+        new_node->next = NULL;
+    }
+};
+
+void display()
+{
+    node *current_node;
+    if (head == NULL)
+    {
+        cout << "Linked list is empty";
+    }
+    else
+    {
+        current_node = head;
+        while (current_node != NULL)
+        {
+            cout << current_node->data << "\t";
+            current_node = current_node->next;
+        }
+    }
 };
